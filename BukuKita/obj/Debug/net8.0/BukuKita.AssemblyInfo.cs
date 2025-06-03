345ba6514cf501e743e6812e98ba7bb9759e3a81
nadyaskr <nadyasekarrahmawati@gmail.com>
2025-06-03 15:37:19 +0700

    menambahkan kelas pengembalian buku

--- BukuKita/obj/Debug/net8.0/BukuKita.AssemblyInfo.cs
+++ BukuKita/obj/Debug/net8.0/BukuKita.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BukuKita")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3313b654a5c6ab5e0fbe376f15a7c048eb4619")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbd375fc982b9e98b2cd99168044e0a87e735709")]
 [assembly: System.Reflection.AssemblyProductAttribute("BukuKita")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BukuKita")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ UnitTest/NadyaSekarRahmawati_Test.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BukuKita.Model;
+using static BookLibrary.BookLib;
+
+namespace UnitTest
+{
+    internal class NadyaSekarRahmawati_Test
+    {
+        [TestMethod]
+        public void Peminjaman()
+        {
+            var buku = new Buku
+            {
+                idBuku = "B001",
+                judul = "Clean Code"
+            };
+            string namaPeminjam = "Andi";
+
+            var peminjaman = new Peminjaman(buku, namaPeminjam);
+            Assert.AreEqual(buku, peminjaman.BukuDipinjam);
+            Assert.AreEqual(namaPeminjam, peminjaman.NamaPeminjam);
+
+            var now = DateTime.Now;
+            Assert.IsTrue((now - peminjaman.TanggalPinjam).TotalSeconds < 5, "TanggalPeminjaman tidak sesuai");
+        }
+
+        [TestMethod]
+        public void Peminjaman_TanggalPeminjaman_IsRecent()
+        {
+            var buku = new Buku { idBuku = "B002", judul = "Refactoring" };
+            var peminjaman = new Peminjaman(buku, "Budi");
+
+            var timeDiff = DateTime.Now - peminjaman.TanggalPinjam;
+            Assert.IsTrue(timeDiff.TotalSeconds < 5, "TanggalPeminjaman harus waktu saat ini");
+        }
+    }
+}
+}
